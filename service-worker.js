a841d2fb049a697ad4ffe262ce2c6e2b66d9c361
asisrasyid <118648153+asisrasyid@users.noreply.github.com>
2025-04-08 10:01:00 +0700

    Add files via upload
    update view scrip dan lain lain (versi terbaru

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,12 @@
+const CACHE_NAME = 'my-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/sweetalert2/sweetalert2.min.js',
+  '/assets/sweetalert2/sweetalert2.min.css',
+  // Tambahkan file lain seperti logo, script, dll.
+];
+
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Installed');
 });
